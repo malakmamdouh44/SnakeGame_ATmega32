04ffbfd7888c78a9e29f56d5e3bdad61b04d9ce6
rema712 <136196991+rema712@users.noreply.github.com>
2026-08-26 16:14:01 +0300

    Add files via upload

---
+++ BUZZER/BUZZER_PRIVATE.h
@@ -0,0 +1,13 @@
+/*
+ * BUZZER_PRIVATE.h
+ *
+ *  Created on: Aug 26, 2026
+ *      Author: reeme
+ */
+
+#ifndef INCLUDE_HAL_BUZZER_BUZZER_PRIVATE_H_
+#define INCLUDE_HAL_BUZZER_BUZZER_PRIVATE_H_
+
+
+
+#endif /* INCLUDE_MCAL_BUZZER_BUZZER_PRIVATE_H_ */
