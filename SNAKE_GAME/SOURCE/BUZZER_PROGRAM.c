d3e998772ec87235d4bf2b8caf126ce57069b7da
rema712 <136196991+rema712@users.noreply.github.com>
2026-08-27 14:48:00 +0300

    BUZZER_PROGRAM.c

--- SNAKE_GAME/SOURCE/BUZZER_PROGRAM.c
+++ SNAKE_GAME/SOURCE/BUZZER_PROGRAM.c
@@ -1,9 +1,50 @@
 /*
  * BUZZER_PROGRAM.c
- *
- *  Created on: 26Aug.,2026
- *      Author: malak
  */
 
+#include <util/delay.h>
+
+#include "../INCLUDE/LIB/STD_TYPES.h"
+#include "../INCLUDE/MCAL/DIO/DIO_INTERFACE.h"
+
+#include "../INCLUDE/HAL/BUZZER/BUZZER_INTERFACE.h"
+#include "../INCLUDE/HAL/BUZZER/BUZZER_CFG.h"
 
 
+void HBuzzer_voidInit(void)
+{
+	MDIO_voidSetPinDirection(
+            BUZZER_PORT,
+            BUZZER_PIN,
+            DIO_OUTPUT);
+
+    HBuzzer_voidOff();
+}
+
+
+void HBuzzer_voidOn(void)
+{
+    MDIO_voidSetPinValue(
+            BUZZER_PORT,
+            BUZZER_PIN,
+            DIO_HIGH);
+}
+
+
+void HBuzzer_voidOff(void)
+{
+    MDIO_voidSetPinValue(
+            BUZZER_PORT,
+            BUZZER_PIN,
+            DIO_LOW);
+}
+
+
+void HBuzzer_voidBeep(void)
+{
+    HBuzzer_voidOn();
+
+    _delay_ms(100);
+
+    HBuzzer_voidOff();
+}
